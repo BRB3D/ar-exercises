f7f6b84a666c7846e055a920f2b7b3bf7f738575
BRB3D <samuraix221@hotmail.com>
2022-01-04 15:40:34 -0500

    Validations including a custom one added, error messages printed too

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,37 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# class Employee < ApplicationRecord
+#   validates :first_name
+#   validates :last_name
+#   validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+#   validates :store_id
+# end
+
+# class Store < ApplicationRecord
+#   validates :name, length: {minimum: 3}
+#   validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+# end
+
+# class ApparelValidator < ActiveModel::Validator
+#   def validate(record)
+#     unless record.mens_apparel || record.womens_apparel
+#       record.errors.add attribute, (options[:message] || "Needs to have etiehr mens or womens apparel")
+#     end
+#   end
+# end
+
+# class Store 
+#   include ActiveModel::Validator
+#   validates_with ApparelValidator
+# end
+
+puts "please enter a store Name"
+print "> "
+
+store_name = gets.chomp
+
+n = Store.create(name: store_name)
+n.valid?
+puts n.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
+
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :appareal_validator
+
+  def appareal_validator
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "Must have Men's apparel or Women's apparel")
+      errors.add(:womens_apparel, "Must have Men's apparel or Women's apparel")
+    end
+  end
 end
