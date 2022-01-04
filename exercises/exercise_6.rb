4a72dd6e33c3f486be88de9b9e63aa54cab81089
BRB3D <samuraix221@hotmail.com>
2022-01-04 14:35:40 -0500

    Added employees by relating the classes and then assigning them to two stroes

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: 'Jose', last_name: "Islas", hourly_rate: 60)
+@store1.employees.create(first_name: 'Sofiya', last_name: "Lipova", hourly_rate: 40)
+@store1.employees.create(first_name: 'Tedd', last_name: "Lasso", hourly_rate: 30)
+
+
+@store2.employees.create(first_name: 'Nico', last_name: "Alkurry", hourly_rate: 20)
+@store2.employees.create(first_name: 'Han', last_name: "Solo", hourly_rate: 50)
+@store2.employees.create(first_name: 'Boba', last_name: "Fett", hourly_rate: 30)
