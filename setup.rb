0f0a77c91833d0443d1282a9eb0c54baa7838689
BRB3D <samuraix221@hotmail.com>
2022-01-04 12:36:09 -0500

    Had some trouble setting up since i needed to add a dependency because im working with ruby 2.7"

--- setup.rb
+++ setup.rb
@@ -10,8 +10,8 @@ puts 'Establishing connection to database ...'
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   database: 'ar_exercises',
-  username: 'development',
-  password: 'development',
+  username: 'labber',
+  password: '1',
   host: 'localhost',
   port: 5432,
   pool: 5,
