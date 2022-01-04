eeb1c50ebc48f979d63488f4e69c44adb5cd4418
BRB3D <samuraix221@hotmail.com>
2022-01-04 13:32:36 -0500

    Different ways of creating used in this exercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,22 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.create do |s|
+s.name = "Burnaby"
+s.annual_revenue = 300000
+s.mens_apparel = true
+s.womens_apparel = true
+end
+
+
+rich = Store.create(name: 'Richmond', annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
+
+gas = Store.new
+gas.name = "Gastown"
+gas.annual_revenue = 190000
+gas.mens_apparel = true
+gas.womens_apparel = false
+gas.save 
+
+puts Store.count
